6506f82412287899b2901fce4bd3ba5c8c3b2c31
makekop <72009584+makekop@users.noreply.github.com>
2025-03-28 16:03:52 +0200

    Update CloudSpeechGIT.py
    Removed comments to avoid confusion.

--- CloudSpeechGIT.py
+++ CloudSpeechGIT.py
@@ -1,50 +1,40 @@
 import os 
-from google.cloud import texttospeech # Import Text-To-Speech API from Google Cloud
+from google.cloud import texttospeech 
 
-# Verify your Google Cloud Credentials with using os.environ 
-credential_path = "demofile/authorization.json" # Replace this part with your own file path
+credential_path = "demofile/authorization.json" 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = credential_path
 
-# Set up client
 client = texttospeech.TextToSpeechClient()
 
-# Read the text file from the file path you've set
-text_path = "demofile/textfile.txt" #Replace this part with your own file path
+text_path = "demofile/textfile.txt"
 with open(text_path, "r", encoding="utf-8") as text_file:
     text_block = text_file.read().strip()
 
-# Define the text input
 synthesis_input = texttospeech.SynthesisInput(text=text_block)
 
-# Select voice
 voice = texttospeech.VoiceSelectionParams(
     language_code="en-US",
     name="en-US-Studio-O"
 )
 
-# Select audio format
 audio_config = texttospeech.AudioConfig(
     audio_encoding=texttospeech.AudioEncoding.MP3,
     speaking_rate=1,
     pitch=1
 )
 
-# Perform the text-to-speech request
 response = client.synthesize_speech(
     input=synthesis_input, 
     voice=voice, 
     audio_config=audio_config
 )
 
-# Create an output file path where you want your audio files saved
-output_folder = "demofile/audiofiles" # Replace this part with your own path
+output_folder = "demofile/audiofiles"
 
-# Get a unique filename 
 def get_unique_filename(base_filename="output.mp3"):
     filename, extension = os.path.splitext(base_filename)
     counter = 1
 
-# Add folder path and generate unique filename inside that folder
     while os.path.exists(os.path.join(output_folder, base_filename)):
         base_filename = f"{filename}_{counter}{extension}"
         counter += 1
@@ -52,9 +42,7 @@ def get_unique_filename(base_filename="output.mp3"):
 
 unique_filename = get_unique_filename("output.mp3")
 
-# Save the audio to a file
 with open(unique_filename, "wb") as out:
     out.write(response.audio_content)
 
-# Print the filename in console
 print(f"Audio file saved as {unique_filename} ")
